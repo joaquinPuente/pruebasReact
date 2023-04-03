5ea88822c606781c5a6f722b61189e2d0be05f4c
Joaquin Puente <joaquinpuentecoronel@gmail.com>
2023-04-03 17:19:15 -0300

    Use useState() y module.css

--- src/assets/Components/HookUseState.jsx
+++ src/assets/Components/HookUseState.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { useState } from 'react';
+import Estilo from './HookUseState.module.css'
 
 const HookUseState = () => {
     const [count, setCount] = useState(0);
   
     return (
-    <div>
+    <div className={Estilo.div}>
     
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
+      <button className={Estilo.button1} onClick={() => setCount(count + 1)}>
+        Click me to +
+      </button>
+
+      <button className={Estilo.button2} onClick={() => setCount(count - 1)}>
+        Click me to -
       </button>
     
     </div>
